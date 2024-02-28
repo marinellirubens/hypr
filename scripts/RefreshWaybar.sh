e6c1f135d6dda0c6af90070c9546c55ebfa5099d
rferrei3 <rubens.ferreira@alticeusa.com>
2024-02-28 10:03:38 -0300

    Adding script to refresh only waybar

---
+++ scripts/RefreshWaybar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Kill already running processes
+_ps=(waybar rofi swaync)
+for _prs in "${_ps[@]}"; do
+    if pidof "${_prs}" >/dev/null; then
+        pkill "${_prs}"
+    fi
+done
+
+sleep 0.3
+# Relaunch waybar
+waybar &
