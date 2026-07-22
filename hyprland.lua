7f706646f4a85f8116a9169f1e1cb661013d0207
marinellirubens <marinelli.rubens@gmail.com>
2026-07-22 05:37:26 -0300

    saving current config

--- hyprland.lua
+++ hyprland.lua
@@ -89,16 +89,13 @@ hl.config({
     general = {
         gaps_in  = 5,
         gaps_out = 10,
-
         border_size = 2,
-
         col = {
             active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
             inactive_border = "rgba(595959aa)",
         },
-
         -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
-        resize_on_border = false,
+        resize_on_border = true,
 
         -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
         allow_tearing = false,
